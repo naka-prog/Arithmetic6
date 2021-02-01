a14db4a056bf556b2b8e6f3f05ae4cd6a0f4f7b6
naka-prog <koji773855@ruby.plala.or.jp>
2021-02-01 11:28:33 +0900

    Initial Commit

---
+++ Arithmetic6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Arithmetic6
+//
+//  Created by koji nakashima on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
