5e8a9563c5ec9240f750ad0d8e9c566306a224d6
naka-prog <koji773855@ruby.plala.or.jp>
2021-02-03 08:59:21 +0900

    メソッドを追加

--- Arithmetic6/ViewController.swift
+++ Arithmetic6/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  Arithmetic6
+//  Arithmetic
 //
-//  Created by koji nakashima on 2021/02/01.
+//  Created by koji nakashima on 2021/01/22.
 //
 
 import UIKit
@@ -13,7 +13,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    // 小学１年生
+    @IBAction func grade1Button(_ sender: UIButton) {
+        performSegue(withIdentifier: "Grade1ndView", sender:nil)
+    }
+    // 小学２年生
+    @IBAction func grade2Button(_ sender: UIButton) {
+        performSegue(withIdentifier: "Grade2ndView", sender:nil)
+    }
+    // 小学３年生
+    @IBAction func grade3Button(_ sender: UIButton) {
+        performSegue(withIdentifier: "Grade3ndView", sender:nil)
+    }
+    
+    @IBAction func helpButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "HelpView", sender:nil)
+    }
+    
+    @IBAction func NoticeView(_ sender: UIButton) {
+        performSegue(withIdentifier: "NoticeView", sender:nil)
+    }
+    
 
 }
-
